4473a767de1b60b25fc609abb167520feb0e4292
Gagan Vasanth <Gagan.Vasanth@walmart.com>
2024-06-25 11:43:13 +0530

    day 7: added the onChange functionality and button disbaling

--- src/common/LoginAndSignup.jsx
+++ src/common/LoginAndSignup.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import '../css/LoginAndSignup.css';
 
 const LoginAndSignup = (props) => {
     const navigate = useNavigate();
+    const [userName, setUserName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
+    useEffect(() => {
+        validateAndAllowLogin();
+    }, [email, password, userName])
+
+    const validateAndAllowLogin = () => {
+        const condition = props.heading === 'login' ? email && password && email.length > 10 && password.length >= 8 : email && password && userName && email.length > 10 && password.length >= 8 && userName.length > 6 
+        if(condition) {
+            setIsButtonDisabled(false);  
+        } else {
+            setIsButtonDisabled(true);
+        }
+    }
+
+    
+
   return (
     <div className='ls-page'>
         <div className='ls-container'>
@@ -12,15 +32,15 @@ const LoginAndSignup = (props) => {
                 {props.heading === 'signup' && 
                 <div>
                         <p className='ls-label'>UserName</p>
-                        <input type='text' className='ls-input'/>
+                        <input type='text' className='ls-input' onChange={(e) => setUserName(e.target.value)} value={userName}/>
                     </div>} 
                 <div>
                         <p className='ls-label'>Email</p>
-                        <input type='email' className='ls-input'/>
+                        <input type='email' className='ls-input' onChange={(e) => setEmail(e.target.value)} value={email}/>
                     </div> 
                     <div>
                         <p className='ls-label'>Password</p>
-                        <input type='password' className='ls-input'/>
+                        <input type='password' className='ls-input'onChange={(e) => setPassword(e.target.value)} value={password}/>
                     </div> 
 
                     <div >
@@ -28,9 +48,9 @@ const LoginAndSignup = (props) => {
                         <div className='ls-notify'>New User? <p onClick={() => navigate('/signup')} style={{ marginLeft: '4px', textDecoration: 'underline', cursor: 'pointer', fontWeight: 700}}>Sign up</p></div> : 
                         <div className='ls-notify'>Already a user? <p onClick={() => navigate('/login')} style={{ marginLeft: '4px', textDecoration: 'underline', cursor: 'pointer', fontWeight: 700}}>Login</p></div>}
                     </div>
-                    <div className='ls-button'>
+                    <button className='ls-button' disabled={isButtonDisabled} onClick={(e) => console.log(e)}>
                         {props.heading}
-                    </div>
+                    </button>
                 </div>
         </div>
     </div>
